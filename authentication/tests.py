852eea8d4966131add140fe94af077fa953d7294
munuhee <murichustephen@gmail.com>
2023-10-27 20:29:00 +0300

    update tests: in progress

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,91 +1,28 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework import status
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
-from authentication.models import CustomUser, PasswordResetToken
-from rest_framework_simplejwt.tokens import RefreshToken
 
-class RegistrationViewTests(TestCase):
+User = get_user_model()
+
+class AuthenticationTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.registration_url = reverse('registration')
-        self.valid_registration_data = {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password': 'password123'
+        self.user_data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "testpassword",
         }
+        self.user = User.objects.create_user(**self.user_data)
 
-    def test_register_user(self):
-        response = self.client.post(self.registration_url, self.valid_registration_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(CustomUser.objects.count(), 1)
+    def test_registration(self):
+        response = self.client.post(reverse("registration"), self.user_data, format="json")
+        self.assertEqual(response.status_code, 201)
 
-class LoginViewTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.login_url = reverse('login')
-        self.user = CustomUser.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='password123'
-        )
-        self.valid_login_data = {
-            'username_or_email': 'testuser',
-            'password': 'password123'
-        }
+    def test_login(self):
+        response = self.client.post(reverse("login"), {"username": "testuser", "password": "testpassword"}, format="json")
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_login(self):
-        response = self.client.post(self.login_url, self.valid_login_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class PasswordResetRequestViewTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.password_reset_request_url = reverse('password-reset-request')
-        self.user = CustomUser.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='password123'
-        )
-        self.valid_reset_request_data = {
-            'email': 'testuser@example.com'
-        }
-
-    def test_reset_request(self):
-        response = self.client.post(self.password_reset_request_url, self.valid_reset_request_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class PasswordResetConfirmViewTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.password_reset_confirm_url = reverse('password-reset-confirm', args=['valid_token', '1'])
-        self.user = CustomUser.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='password123'
-        )
-        self.valid_reset_confirm_data = {
-            'password': 'newpassword'
-        }
-
-    def test_reset_confirm(self):
-        response = self.client.put(self.password_reset_confirm_url, self.valid_reset_confirm_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class RefreshTokenViewTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.refresh_token_url = reverse('refresh-token')
-        self.user = CustomUser.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='password123'
-        )
-        self.refresh = RefreshToken.for_user(self.user)
-        self.valid_refresh_data = {
-            'refresh': str(self.refresh)
-        }
-
-    def test_refresh_token(self):
-        response = self.client.post(self.refresh_token_url, self.valid_refresh_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_password_reset_request(self):
+        response = self.client.post(reverse("password-reset-request"), {"email": "test@example.com"}, format="json")
+        self.assertEqual(response.status_code, 200)
